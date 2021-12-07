e02be0f605a11dbdf8b3ccc53c903491c598b78a
lithika-damnod <l.d.b.dasanayaka@gmail.com>
2021-12-07 08:05:24 +0530

    added templates but it's not supporting

--- hackerrank/problem-solving/binaryTree/include/binaryTree.h
+++ hackerrank/problem-solving/binaryTree/include/binaryTree.h
@@ -1,18 +1,19 @@
 #ifndef BINARYTREE_H
 #define BINARYTREE_H
 
-
+template <class T>
 class binaryTree
 {
     public:
-        binaryTree(int val);
+        binaryTree();
+        binaryTree(T val);
         binaryTree *left = nullptr;
         binaryTree *right = nullptr;
         void print();
     protected:
 
     private:
-        int data = 0;
+        T data = 0;
 };
 
 #endif // BINARYTREE_H
--- hackerrank/problem-solving/binaryTree/main.cpp
+++ hackerrank/problem-solving/binaryTree/main.cpp
@@ -5,11 +5,11 @@ using namespace std;
 
 int main()
 {
-    cout << "Binary Tree" << endl;
-    binaryTree *root = new binaryTree(1);
-    root->left = new binaryTree(2);
-    root->right = new binaryTree(3);
-    root->left->left = new binaryTree(4);
+    cout << "Binary Tree - Nodes" << endl;
+    binaryTree *root = new binaryTree("lithika");
+    root->left = new binaryTree("damnod");
+    root->right = new binaryTree("bandara");
+    root->left->left = new binaryTree("dasanayaka");
     root->print();
     return 0;
 }
