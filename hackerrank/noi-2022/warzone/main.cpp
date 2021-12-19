e021e9572ae88d82458a63dee9a51ef8abb91c88
lithika-damnod <l.d.b.dasanayaka@gmail.com>
2021-12-19 21:12:34 +0530

    solution works properly but terminated due to time out

--- hackerrank/noi-2022/warzone/main.cpp
+++ hackerrank/noi-2022/warzone/main.cpp
@@ -5,8 +5,45 @@
 #include <algorithm>
 using namespace std;
 
+// checks for a value in a vector
+bool check(vector<int> arr, int value){
+    if(std::find(arr.begin(), arr.end(), value) != arr.end()) {
+        return true;
+    } else {
+        return false;
+    }
+}
 
 int main() {
-    /* Enter your code here. Read input from STDIN. Print output to STDOUT */
+
+    vector<int> fighters;
+    vector<int> cities;
+    int x, y; cin >> x >> y;
+    // get input for fighter planes
+    for(int i=0; i<x; i++){
+        int tmp;
+        cin >> tmp;
+        fighters.push_back(tmp);
+    }
+    // get input for cities
+    for(int i=0; i<y; i++){
+        int tmp;
+        cin >> tmp;
+        cities.push_back(tmp);
+    }
+    // calculate max deployment cities
+    vector<int> city; // for storing city indexes
+    vector<int> planes; // for storing plane indexes
+    int nCities = 0;
+    for(int i=0; i<fighters.size(); i++){
+        for(int j=0; j<cities.size(); j++){
+            if((fighters[i] + cities[j]) % 2 == 1 && !check(city, j) && !check(planes, i)){
+                city.push_back(j);
+                planes.push_back(i);
+                nCities++;
+            }
+        }
+    }
+    cout << nCities << endl; // print out maximum number of cities
     return 0;
 }
