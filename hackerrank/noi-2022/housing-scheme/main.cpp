a81c5c7392ac351ad1ab75269121c13e6e814576
lithika-damnod <l.d.b.dasanayaka@gmail.com>
2021-12-26 10:12:24 +0530

    to resolve jumbled files

--- hackerrank/noi-2022/housing-scheme/main.cpp
+++ hackerrank/noi-2022/housing-scheme/main.cpp
@@ -5,8 +5,41 @@
 #include <algorithm>
 using namespace std;
 
+void rearrangeBuilding(int n){
+    vector<int> odd;
+    vector<int> even;
+    if(n <= 3)
+        cout << "-1" << endl;
+    else{
+        for(int i=n; i>0; i--){
+            if(i%2 == 0)
+                even.push_back(i);
+            else
+                odd.push_back(i);
+        }
+        // swap two values in the even arr
+        reverse(even.begin(), even.end());
+        int tmp = even[0];
+        even[0] = even[1];
+        even[1] = tmp;
+        // print the array
+        for(int i=0; i<odd.size(); i++)
+            cout << odd[i] << " ";
+        for(int i=0; i<even.size(); i++)
+            cout << even[i] << " ";
+        cout << endl;
+    }
+}
 
 int main() {
-    /* Enter your code here. Read input from STDIN. Print output to STDOUT */
+    int n; cin >> n;
+    vector<int> nT;
+    for(int i=0; i<n; i++){
+        int tmp; cin >> tmp;
+        nT.push_back(tmp);
+    }
+    for(int i=0; i<nT.size(); i++){
+        rearrangeBuilding(nT[i]);
+    }
     return 0;
 }
---
+++ hackerrank/noi-2022/past-papers/2000/palindrome/main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main()
+{
+    int n; cin >> n;
+    char inp[n];
+    for(int i=0; i<n; i++){
+        cin >> inp[i];
+    }
+    return 0;
+}
